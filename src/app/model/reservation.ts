1b644448a30d476fae4a07f16f2096890deb4ffe
haswanth9891 <19bcs071@iiit.ac.in>
2024-06-01 11:01:33 +0530

    first commit

---
+++ src/app/model/reservation.ts
@@ -0,0 +1,13 @@
+export interface Reservation {
+    reservationId: number;
+    checkInDate: String;
+    checkOutDate: String;
+    numberOfAdults: number;
+    numberOfChildren: number;
+    totalAmount: number;
+    reservationStatus: number;
+    refundProcessed: boolean;
+    roomIds: number[];
+    hotelName: String;
+
+}
